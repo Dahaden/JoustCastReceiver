ffc9e63291169eec545fb33883560b06c3a07d60
Dave Haden <dahaden@gmail.com>
2015-06-10 08:20:09 +1000

    Give this a try

--- bare_listener.js
+++ bare_listener.js
@@ -6,10 +6,14 @@ window.onload = function () {
     window.gameManager = new cast.receiver.games.GameManager(gameConfig);
     
     gameManager.addEventListener(cast.receiver.games.EventType.PLAYER_AVAILABLE, function (event) {
-        var result = { type: 2, 'isHost': false };
+        var result = { type: 2, 'isHost': true };
         gameManager.sendGameMessageToPlayer(event.playerInfo.playerId, result);
     });
 	
+    gameManager.addEventListener(cast.receiver.games.EventType.PLAYER_READY, function (event) {
+        var result = { type: 2, 'isHost': true };
+        gameManager.sendGameMessageToPlayer(event.playerInfo.playerId, result);
+    });
 	
 	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
 	
@@ -20,4 +24,5 @@ window.onload = function () {
     };
 	
 	window.castReceiverManager.start({ statusText: "Application is starting" });
+    gameManager.updateGameStatusText("Ready");
 };
